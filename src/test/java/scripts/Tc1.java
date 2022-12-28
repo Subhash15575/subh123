3cd6873847ba16cb2f90bc66f95366f80bd685e8
sbind <sbind@IN-NPANDEY.mycom-internal.com>
2022-12-28 16:02:39 +0530

    pro

---
+++ src/test/java/scripts/Tc1.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1 {
+	
+	@Test
+	public void demo()
+	{
+		Reporter.log("hello tc1",true);
+	}
+
+}
